71aa2b06ac89deff35ad05a4835a4efb75ee48ce
amit-verma-1093 <amitverma10931@gmail.com>
2026-08-15 08:40:35 +0530

    Add files via upload

---
+++ graet_two.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    int a,b;
+
+    printf("enter two numbers: ");
+    scanf("%d%d",&a,&b);
+
+    if (a>b)
+        printf("%d is the greatest number: ",a);
+    else
+        printf("%d is the greatest number: ",b);
+    return 0;
+}

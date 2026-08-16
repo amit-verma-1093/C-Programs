68a09547079c9931b356045292e47ff6602a222f
amit-verma-1093 <amitverma10931@gmail.com>
2026-08-16 17:01:54 +0530

    Add files via upload

---
+++ month_num.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int month_number;
+    printf("Enter month number (1-12): ");
+    scanf("%d", &month_number);
+
+    if (month_number == 1 || month_number == 3 || month_number == 5 || 
+        month_number == 7 || month_number == 8 || month_number == 10 || 
+        month_number == 12) 
+        printf("31 Days");
+    else if (month_number == 4 || month_number == 6 || 
+               month_number == 9 || month_number == 11) 
+        printf("30 Days");
+    else if (month_number == 2) 
+        printf("28 or 29 Days (Depending on Leap Year)");
+    else 
+        printf("Invalid input! Please enter a number between 1 and 12.");
+
+    return 0;
+}

cd7337aaf2a11322856a60339c2dfc267baccbdc
amit-verma-1093 <amitverma10931@gmail.com>
2026-08-16 16:35:24 +0530

    Add files via upload

---
+++ vote.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("enter your age: ");
+    scanf("%d",&a);
+    if (a>=18)
+        printf("You are eligible for voting");
+    else
+        printf("You are not eligible for voting");
+    return 0;
+ }
